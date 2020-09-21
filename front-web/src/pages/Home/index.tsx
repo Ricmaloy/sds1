222a417c705026a948516421257e0c8e847632d4
Ricardo <ricardo10_2010@hotmail.com>
2020-09-21 18:04:57 -0300

    Adicionando animaçoes :fire:

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import Typical from 'react-typical';
 import { ReactComponent as ArrowIcon } from '../../assets/arrow.svg';
 import { ReactComponent as GamerImage } from '../../assets/gamer.svg';
-import { Link } from 'react-router-dom' 
-import './styles.css'
+import { Link } from 'react-router-dom';
+import '../../animations/animations.css';
+import './styles.css';
 
 const Home = () => (
     <div className="home-container">
         <div className="home-text">
             <h1 className="home-text-title">
-                Quais jogos a galera gosta mais ?
+                Quais 
+                <Typical
+                    loop={Infinity}
+                    wrapper='b'
+                    steps={[
+                        ' jogos a galera gosta mais ?',
+                        2000,
+                        ' os melhores jogos da atualidade ?',
+                        2000
+                    ]}
+                />
             </h1>
             <h3 className="home-text-subtitle">
                 Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo!
